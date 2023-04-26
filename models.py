8d433bf48edb3566e1f93580ed606c64dd1581c0
Francesco Antonetti Lamorgese Passeri <francescoantonetti1@gmail.com>
2023-04-26 11:55:20 +0200

    -

--- models.py
+++ models.py
@@ -6,32 +6,6 @@ from django.db import models
 
 # Create your models here.
 
-class Grids(models.Model):
-    name = models.CharField(max_length=26)
-    models.UniqueContraint(fields=['name'],name='unique_grid_name')
-
-class Simulators(models.Model):
-    name = models.CharField(max_length=26)
-    models.UniqueContraint(fields=['name'],name='unique_simulator_name')
-
-class Regions(models.Model):
-    regionName = models.CharField(max_length=26)
-    simulator  = models.CharField(max_length=26)
-    grid       = models.CharField(max_length=26)
-    models.UniqueConstraint(fields=['regionName','simulator'],name='unique_region_in_grid_simulator')
-
-class Avatars(models.Model):
-    firstName = models.CharField(max_length=26)
-    lastName  = models.CharField(max_length=26)
-    username  = models.CharField(max_length=26)
-    models.UniqueConstraint(fields=['firstName','lastName','username'],name='unique_user')
-
-class OwnerRegionAssociations(models.Model):
-    username = models.CharField(max_length=26)
-    region   = models.CharField(max_length=26)
-    grid     = models.CharField(max_length=26)
-    models.UniqueConstraint(fields=['username','region','grid'],name='unique_user_region_association')
-
 class RESTConsoles(models.Model):
     simulator = models.CharField(max_length=26)
     ip        = models.CharField(max_length=26)
